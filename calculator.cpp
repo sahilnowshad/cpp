7f6bbed1792805cafb066c68ffae5d819ddbfb93
sahil nowshad turk <sahil.nowshad333@gmail.com>
2025-04-16 19:10:14 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,41 +1,55 @@
 #include<iostream>
-  using namespace std;
-      int sum(int n1,int n2){
-        return n1+n2;
-      }
-        int difference(int n1,int n2){
-            return n1-n2;
+using namespace std;
 
-        }
-          int difference(int n1,int n2){
-              return n1-n2;
-          }
-            int multiplication(int n1,int n2){
-                return n1*n2;
-            }
-              int division(int n1,int n2){
-                return n1/n2;
-              }
-              int main(){
-                int n1,n2;
-                char ch;
-                 cout<<"enter the first number n1";
-                 cin>>n1;
-                    cout<<"enter the second number n2";
-                    cin>>n2;
-                    switch(ch){
-                        case 1:
-                         cout<<"the sum is equal to"<<sum(n1,n2)<<endl;
-                         break;
-                           case 2:
-                             cout<<"the difference of two numbers is"<<difference(n1,n2)<<endl;
-                               break;
-                               case 3:
-                               cout<<"the multiplication if two no is"<<multiplication(n1,n2)<<endl;
-                                 break;
-                         case 4:
-                         cout<<"the division of two no is "<<division(n1,n2)<<endl;  
-                         break;      
-                    }
+int sum(int n1, int n2) {
+    return n1 + n2;
+}
 
-              }
+int difference(int n1, int n2) {
+    return n1 - n2;
+}
+
+int multiplication(int n1, int n2) {
+    return n1 * n2;
+}
+
+int division(int n1, int n2) {
+    if (n2 == 0) {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+    }
+    return n1 / n2;
+}
+
+int main() {
+    int n1, n2, choice;
+
+    cout << "Enter the first number (n1): ";
+    cin >> n1;
+    cout << "Enter the second number (n2): ";
+    cin >> n2;
+
+    cout << "Choose operation:\n";
+    cout << "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+            cout << "The sum is: " << sum(n1, n2) << endl;
+            break;
+        case 2:
+            cout << "The difference is: " << difference(n1, n2) << endl;
+            break;
+        case 3:
+            cout << "The multiplication is: " << multiplication(n1, n2) << endl;
+            break;
+        case 4:
+            cout << "The division is: " << division(n1, n2) << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+    }
+
+    return 0;
+}
